c3f8975fd1b68b9eccf69298389cd411e5c1e058
Mohammednamara <mohammedhnamara@gmail.com>
2022-11-10 11:02:33 +0200

    Multiblication Table

---
+++ Multiblication_table.py
@@ -0,0 +1,17 @@
+
+#Multiblication table 1*8
+x=1
+while x<=8:
+    print(f'1 X {x} = {x*1}')
+    x+=1
+-----------
+#Multiblication table 5*5
+Mt=1
+while Mt<=5:
+    y=1
+    while y<=5:
+        print(f'{Mt} X {y} = {Mt*y}')
+        y+=1
+    Mt+=1
+
+
