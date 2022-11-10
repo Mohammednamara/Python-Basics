33c5d55cb60d86ce22fdc91e6c1308156c1dc0b4
Mohammednamara <mohammedhnamara@gmail.com>
2022-11-10 10:34:38 +0200

    Loop

---
+++ Loop.py
@@ -0,0 +1,24 @@
+'''
+#While loop Example
+x=0
+while x<10:
+    print(x)
+    x +=1
+
+#Infinite loop
+x=0
+while x<10:
+    print(x)
+    ---
+
+x=0
+while x<10: print(x);x+=1
+ '''
+#Python nested while loop example
+x=0
+while x<10:
+    y=0
+    while y<5:
+        print(x,y)
+        y+=1
+    x+=1
